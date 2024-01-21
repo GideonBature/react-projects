27459f68a33ee0fb2e2015c2501e413daa542d26
GideonBature <infoaboutgideon@gmail.com>
2024-01-21 14:31:30 +0100

    add useState to change the state of the button

--- counter_app/src/components/Counter/Counter.jsx
+++ counter_app/src/components/Counter/Counter.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus, faCircleMinus, faRotateRight } from '@fortawesome/free-solid-svg-icons'
 import TabButton from '../TabButton/TabButton';
 
 function Counter() {
+    const [count, setCount] = useState(0);
 
     function handleClick(buttonClicked) {
-        console.log(buttonClicked);
+        if (buttonClicked === 'increment') {
+            setCount(count + 1)
+        } else if (buttonClicked === 'decrement') {
+            setCount(count - 1)
+        } else if (buttonClicked === 'reset') {
+            setCount(0)
+        }
     }
     return (
         <div className="bg-gray-900 h-screen flex">
-            <div className="w-1/2 m-auto py-10 bg-violet-500 flex flex-col items-center">
+            <div className="w-1/2 m-auto py-10 bg-violet-500 flex flex-col items-center rounded">
                 <h3 className="text-2xl font-bold text-white mb-4">Counter</h3>
-                <h1 className="text-7xl font-bold text-white">0</h1>
+                <h1 className="text-7xl font-bold text-white">{count}</h1>
                 <hr className="w-3/4 m-4"/>
                 <div className="flex justify-between w-3/4 m-5">
                     <TabButton onSelect={() => handleClick('decrement')} className="bg-red-600 p-2 text-white rounded hover:bg-red-700"> <FontAwesomeIcon icon={faCircleMinus} />  Decrement</TabButton>
